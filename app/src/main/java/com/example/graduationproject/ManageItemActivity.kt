5e6126a0456aa688eaa03302647c32064ea2b0b0
GirlsLikeMeY <psr2560@naver.com>
2022-02-25 15:13:54 +0900

    update managing item xml

---
+++ app/src/main/java/com/example/graduationproject/ManageItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.graduationproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_item)
+    }
+}
--- app/src/main/java/com/example/graduationproject/UserSigning/LoginActivity.kt
+++ app/src/main/java/com/example/graduationproject/UserSigning/LoginActivity.kt
@@ -8,11 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.graduationproject.databinding.ActivityLoginBinding
-import com.example.graduationproject.databinding.ActivityMainBinding
 
 class LoginActivity : Fragment() {
     private var binding1: ActivityLoginBinding? = null
     private val binding get()= binding1!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +25,7 @@ class LoginActivity : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
 
 
