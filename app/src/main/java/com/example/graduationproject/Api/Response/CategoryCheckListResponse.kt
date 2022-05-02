08cba13aba7f214850f71342ef9c840a66ecd80c
GirlsLikeMeY <psr2560@naver.com>
2022-05-02 22:37:29 +0900

    물품 체크리스트 리사이클러뷰 연결 전(#6)

---
+++ app/src/main/java/com/example/graduationproject/Api/Response/CategoryCheckListResponse.kt
@@ -0,0 +1,17 @@
+package com.example.graduationproject.Api.Response
+
+import com.google.gson.annotations.SerializedName
+
+data class CategoryCheckListResponse (
+    @SerializedName("categoryId")
+    val categoryId:Long,
+    @SerializedName("categoryName")
+    val categoryName:String,
+    @SerializedName("productImgUrl")
+    val productImgUrl:String,
+    @SerializedName("productName")
+    val productName:String,
+    @SerializedName("productPrice")
+    val productPrice:Int
+
+        )
---
+++ app/src/main/java/com/example/graduationproject/Owner/ChecklistActivity.kt
@@ -0,0 +1,10 @@
+package com.example.graduationproject.Owner
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.graduationproject.databinding.ActivityChecklistBinding
+
+class ChecklistActivity:AppCompatActivity() {
+    private lateinit var binding: ActivityChecklistBinding
+
+
+}
