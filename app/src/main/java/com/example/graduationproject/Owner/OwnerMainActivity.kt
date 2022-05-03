b661aada5ba3c1d19f05ea8f36184d245e4e7c3d
JK_ <74819863+jin8073@users.noreply.github.com>
2022-05-03 23:51:06 +0900

    마트 등록되어 있을 경우 마트 등록 버튼 보이지 않게 수정(#13)

--- app/src/main/java/com/example/graduationproject/Owner/OwnerMainActivity.kt
+++ app/src/main/java/com/example/graduationproject/Owner/OwnerMainActivity.kt
@@ -2,20 +2,30 @@ package com.example.graduationproject.Owner
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.graduationproject.Api.Response.GetMartInfoResponse
 import com.example.graduationproject.MainActivity
 import com.example.graduationproject.R
+import com.example.graduationproject.User.AddHeaderJWT
 import com.example.graduationproject.User.OrderlistActivity
 import com.example.graduationproject.UserSigning.SelectWhatToLoginActivity
 import com.example.graduationproject.databinding.ActivityOwnermainBinding
 import com.google.android.material.navigation.NavigationView
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import java.util.concurrent.TimeUnit
 
 class OwnerMainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityOwnermainBinding
-    var martexist = 0   //마트 등록 여부 (0 -> 등록 안함, 1 -> 등록 함)
 
     lateinit var navigationView: NavigationView
 
@@ -25,10 +35,49 @@ class OwnerMainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         setContentView(binding.root)
         actionBar?.hide()
 
-        val sharedPreferences = getSharedPreferences("userInfo", MODE_PRIVATE)
-        val userName = sharedPreferences.getString("userName", "")
+        val pref = getSharedPreferences("userInfo", MODE_PRIVATE)
+        val userName = pref.getString("userName", "")
 
         binding.omainUsername.setText(userName)
+
+        // 로그인 후 저장해둔 JWT 토큰 가져오기
+        val sharedPreferences = getSharedPreferences("token", MODE_PRIVATE)
+        val jwt = sharedPreferences.getString("jwt", "")
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AddHeaderJWT(jwt.toString())) // JWT header 달아주는 interceptor 추가
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://13.124.13.202:8080/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client).build()
+
+        val service = retrofit.create(GetMartService::class.java)
+
+       service.get_mart()
+           .enqueue(object : Callback<GetMartInfoResponse> {
+               override fun onResponse(
+                   call: Call<GetMartInfoResponse>,
+                   response: Response<GetMartInfoResponse>
+               ) {
+                   if(response.isSuccessful) {
+                       val result = response.body()
+                       Log.e("조회 완료", "${result}")
+                       if (result?.data != null) {
+                           binding.setmartBtn.visibility = View.INVISIBLE
+                       }
+                   } else {
+                       Log.d("마트 조회", "실패")
+                   }
+               }
+
+               override fun onFailure(call: Call<GetMartInfoResponse>, t: Throwable) {
+                   Log.e("연결실패", t.message.toString())
+               }
+           })
 
 
         // 햄버거 클릭시 메뉴
@@ -38,15 +87,10 @@ class OwnerMainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
 
         //마트 등록을 요구하는 버튼을 누를시 등록화면으로 이동
         binding.setmartBtn.setOnClickListener {
-            martexist = 1
             val intent = Intent(this, SetMartActivity::class.java)
             startActivity(intent)
         }
 
-        //마트가 등록되어 있으면 등록을 요구하는 버튼이 안보이게 됨
-        if(martexist != 0){
-            binding.setmartBtn.setVisibility(View.GONE)
-        }
 
         //상품 등록
         binding.omainSetP.setOnClickListener {
@@ -95,4 +139,9 @@ class OwnerMainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         }
         return false
     }
+}
+
+interface GetMartService {
+    @GET("mart/info")
+    fun get_mart():Call<GetMartInfoResponse>
 }
